5eee7f6ee7ed4903489083d5d0c90f0d1fa50f05
gianlucaparadise <gianlucaparadise@gmail.com>
2021-11-12 06:37:36 +0100

    iOS: Include pigeons in Xcode proj

--- makePigeon.sh
+++ makePigeon.sh
@@ -1,7 +1,7 @@
 flutter pub run pigeon \
 --input pigeon/host_api_definitions.dart \
 --dart_out lib/host_api.dart \
---objc_header_out ios/Classes/HostApi.h \
---objc_source_out ios/Classes/HostApi.m \
+--objc_header_out ios/Runner/HostApi.h \
+--objc_source_out ios/Runner/HostApi.m \
 --java_out ./android/app/src/main/java/com/example/pigeon_issue_example/HostApi.java \
 --java_package "com.example.pigeon_issue_example"
